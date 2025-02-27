15235ea26602ec87c909ea513cdaf7dbab3f0e91
Luis Filipe Alves <lfilipealves20@gmail.com>
2025-02-27 08:15:28 -0300

    atualização

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -1,7 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const axios = require("axios");
 
-
 const prisma = new PrismaClient();
 
 async function searchAddress(cep) {
@@ -12,10 +11,10 @@ async function searchAddress(cep) {
 
     return {
       street: response.data.logradouro || "",
-      neighborhood: response.data.bairro|| "",
-      city: response.data.localidade|| "",
-      state: response.data.uf|| "",
-      country: "Brasil"
+      neighborhood: response.data.bairro || "",
+      city: response.data.localidade || "",
+      state: response.data.uf || "",
+      country: "Brasil",
     };
   } catch (error) {
     console.error("Erro ao buscar o CEP:", error.message);
@@ -24,93 +23,87 @@ async function searchAddress(cep) {
 }
 
 async function main() {
-
-
   const Usersdata = [
     {
       name: "Luis Filipe",
       email: "luis.rufino@exemplo.com",
       addresses: [
-        { cep: "37074010", title: "Casa", number: 250},
+        { cep: "37074010", title: "Casa", number: 250 },
 
-        {cep: "37070450", title: "Trabalho", number: 147}
-      ]
+        { cep: "37010640", title: "Trabalho", number: 147 },
+      ],
     },
 
     {
       name: "Ana Carolina",
       email: "ana.carolina@exemplo.com",
       addresses: [
-        { cep: "37074010", title: "Casa", number: 250},
-        {cep: "37070450", title: "Escola", number: 155}
-      ]
+        { cep: "37074010", title: "Casa", number: 250 },
+        { cep: "37070450", title: "Escola", number: 155 },
+      ],
     },
   ];
 
   for (const userData of Usersdata) {
     let user = await prisma.user.findUnique({
-      where: { email: userData.email}
+      where: { email: userData.email },
     });
-    
-    if(!user){
-    const user = await prisma.user.create({
-      data: {
-        name: userData.name,
-        email: userData.email,
-        addresses: {
-          create: await Promise.all(
-            userData.addresses.map(async (addressData) => {
-              const addressInfo = await searchAddress(addressData.cep);
-              return {
-                zipcode: addressData.cep,
-                street: addressInfo.street,
-                neighborhood: addressInfo.neighborhood,
-                city: addressInfo.city,
-                state: addressInfo.state,
-                country: addressInfo.country,
-                number: addressData.number,
-                title: addressData.title
-              };
-            })
-          ),
+
+    if (!user) {
+      const user = await prisma.user.create({
+        data: {
+          name: userData.name,
+          email: userData.email,
+          addresses: {
+            create: await Promise.all(
+              userData.addresses.map(async (addressData) => {
+                const addressInfo = await searchAddress(addressData.cep);
+                return {
+                  zipcode: addressData.cep,
+                  street: addressInfo.street,
+                  neighborhood: addressInfo.neighborhood,
+                  city: addressInfo.city,
+                  state: addressInfo.state,
+                  country: addressInfo.country,
+                  number: addressData.number,
+                  title: addressData.title,
+                };
+              })
+            ),
+          },
         },
-      },
-      include: { addresses:true},
-    });
-    console.log(`Usuário ${user.name} inserido!`);
-
+        include: { addresses: true },
+      });
+      console.log(`Usuário ${user.name} inserido!`);
+    }
   }
+  console.log("Processo de seed concluído");
 }
-console.log("Processo de seed concluído");
-}
-
 
 main()
-.catch((e) => console.error(e))
-.finally(() => prisma.$disconnect());
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
 
-    // for (const addressData of userData.addresses){
-    //   const addressInfo = await searchAddress(addressData.cep)
+// for (const addressData of userData.addresses){
+//   const addressInfo = await searchAddress(addressData.cep)
 
-    //   if(addressInfo){
-    //     await prisma.address.create({
-    //       data: {
-    //         user: { connect: {id: user.id}},
-    //         zipcode: addressData.cep,
-    //         street: addressInfo.street,
-    //         neighborhood: addressInfo.neighborhood,
-    //         state: addressInfo.state,
-    //         country: addressInfo.country,
-    //         number: addressData.number,
-    //         title: addressData.title
-    //       }
-    //     }),
+//   if(addressInfo){
+//     await prisma.address.create({
+//       data: {
+//         user: { connect: {id: user.id}},
+//         zipcode: addressData.cep,
+//         street: addressInfo.street,
+//         neighborhood: addressInfo.neighborhood,
+//         state: addressInfo.state,
+//         country: addressInfo.country,
+//         number: addressData.number,
+//         title: addressData.title
+//       }
+//     }),
 
-    //     console.log(`Endereço ${addressData.title} inserido para ${user.name}`);
-    //   }
-    // }
-
-
+//     console.log(`Endereço ${addressData.title} inserido para ${user.name}`);
+//   }
+// }
 
 //   const userWithAddress = await Promise.all(
 //     Usersdata.map(async (user) => {
@@ -126,18 +119,18 @@ main()
 //           country: endereco?.país || null
 //         };
 //       })
-      
+
 //     );
 
 //     return {
 //       ...user,
 //       addresses: updateAddresses,
 //     };
-  
+
 //     })
-    
+
 // );
-      
+
 //      for(const user of userWithAddress) {
 //       const createdUser = await prisma.user.create({
 //         data: {
@@ -157,4 +150,3 @@ main()
 //   });
 //   console.log("Usuários inseridos!");
 // }
-
